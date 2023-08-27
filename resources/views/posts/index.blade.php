ebe7a52ff0051df4d27b609ce5f5a4c48d725900
CodingWithAbdou <coder.abdou@gmail.com>
2023-08-27 11:56:30 +0100

    - Create Path & Page For Create Blog & Page Showing The Pos

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,270 @@
+
+<!doctype html>
+<html lang="en" dir="rtl">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Blog Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    link
+    <!-- Custom styles for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700,900" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.rtl.min.css" integrity="sha384-gXt9imSW0VcJVHezoNQsP+TNrjYXoGcrqBZJpry9zJt8PCQjobwmhMGaDHTASo9N" crossorigin="anonymous">
+
+    <style>
+        /* stylelint-disable selector-list-comma-newline-after */
+
+            .blog-header {
+            line-height: 1;
+            border-bottom: 1px solid #e5e5e5;
+            }
+
+            .blog-header-logo {
+            font-family: "Playfair Display", Georgia, "Times New Roman", serif;
+            font-size: 2.25rem;
+            }
+
+            .blog-header-logo:hover {
+            text-decoration: none;
+            }
+
+            h1, h2, h3, h4, h5, h6 {
+            font-family: "Playfair Display", Georgia, "Times New Roman", serif;
+            }
+
+            .display-4 {
+            font-size: 2.5rem;
+            }
+            @media (min-width: 768px) {
+            .display-4 {
+                font-size: 3rem;
+            }
+            }
+
+            .nav-scroller {
+            position: relative;
+            z-index: 2;
+            height: 2.75rem;
+            overflow-y: hidden;
+            }
+
+            .nav-scroller .nav {
+            display: -webkit-box;
+            display: -ms-flexbox;
+            display: flex;
+            -ms-flex-wrap: nowrap;
+            flex-wrap: nowrap;
+            padding-bottom: 1rem;
+            margin-top: -1px;
+            overflow-x: auto;
+            text-align: center;
+            white-space: nowrap;
+            -webkit-overflow-scrolling: touch;
+            }
+
+            .nav-scroller .nav-link {
+            padding-top: .75rem;
+            padding-bottom: .75rem;
+            font-size: .875rem;
+            }
+
+            .card-img-right {
+            height: 100%;
+            border-radius: 3px 0 0 3px;
+            }
+
+            .flex-auto {
+            -ms-flex: 0 0 auto;
+            -webkit-box-flex: 0;
+            flex: 0 0 auto;
+            }
+
+            .h-250 { height: 250px; }
+            @media (min-width: 768px) {
+            .h-md-250 { height: 250px; }
+            }
+
+            .border-top { border-top: 1px solid #e5e5e5; }
+            .border-bottom { border-bottom: 1px solid #e5e5e5; }
+
+            .box-shadow { box-shadow: 0 .25rem .75rem rgba(0, 0, 0, .05); }
+
+            /*
+            * Blog name and description
+            */
+            .blog-title {
+            margin-bottom: 0;
+            font-size: 2rem;
+            font-weight: 400;
+            }
+            .blog-description {
+            font-size: 1.1rem;
+            color: #999;
+            }
+
+            @media (min-width: 40em) {
+            .blog-title {
+                font-size: 3.5rem;
+            }
+            }
+
+            /* Pagination */
+            .blog-pagination {
+            margin-bottom: 4rem;
+            }
+            .blog-pagination > .btn {
+            border-radius: 2rem;
+            }
+
+            /*
+            * Blog posts
+            */
+            .blog-post {
+            margin-bottom: 4rem;
+            }
+            .blog-post-title {
+            margin-bottom: .25rem;
+            font-size: 2.5rem;
+            }
+            .blog-post-meta {
+            margin-bottom: 1.25rem;
+            color: #999;
+            }
+
+            /*
+            * Footer
+            */
+            .blog-footer {
+            padding: 2.5rem 0;
+            color: #999;
+            text-align: center;
+            background-color: #f9f9f9;
+            border-top: .05rem solid #e5e5e5;
+            }
+            .blog-footer p:last-child {
+            margin-bottom: 0;
+            }
+
+    </style>
+    </head>
+
+    <body>
+        <div class="container">
+            {{-- <h1>{{ $post->title }}</h1> --}}
+        <header class="blog-header py-3">
+            <div class="row flex-nowrap justify-content-between align-items-center">
+            <div class="col-4 pt-1">
+                <a class="text-muted" href="#">Subscribe</a>
+            </div>
+            <div class="col-4 text-center">
+                <a class="blog-header-logo text-dark" href="#">Large</a>
+            </div>
+            <div class="col-4 d-flex justify-content-end align-items-center">
+                <a class="text-muted" href="#">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mx-3"><circle cx="10.5" cy="10.5" r="7.5"></circle><line x1="21" y1="21" x2="15.8" y2="15.8"></line></svg>
+                </a>
+                <a class="btn btn-sm btn-outline-secondary" href="#">Sign up</a>
+            </div>
+            </div>
+        </header>
+
+        <div class="nav-scroller py-1 mb-2">
+            <nav class="nav d-flex justify-content-between">
+            <a class="p-2 text-muted" href="#">World</a>
+            <a class="p-2 text-muted" href="#">U.S.</a>
+            <a class="p-2 text-muted" href="#">Technology</a>
+            <a class="p-2 text-muted" href="#">Design</a>
+            <a class="p-2 text-muted" href="#">Culture</a>
+            <a class="p-2 text-muted" href="#">Business</a>
+            <a class="p-2 text-muted" href="#">Politics</a>
+            <a class="p-2 text-muted" href="#">Opinion</a>
+            <a class="p-2 text-muted" href="#">Science</a>
+            <a class="p-2 text-muted" href="#">Health</a>
+            <a class="p-2 text-muted" href="#">Style</a>
+            <a class="p-2 text-muted" href="#">Travel</a>
+            </nav>
+        </div>
+
+
+
+        </div>
+
+        <main role="main" class="container">
+            <div class="row">
+                <div class="col-md-8 blog-main">
+                <h3 class="pb-3 mb-4 font-italic border-bottom">
+                    From the Firehose
+                </h3>
+
+                <?php foreach ($posts as $post ) : ?>
+                    <div class="blog-post">
+                        <a href="/posts/<?= $post->id ?>">
+                            <h2 class="blog-post-title">
+                                <?= $post->title ?>
+                            </h2>
+                        </a>
+                        <p class="blog-post-meta">
+                            <a href="#">بقلم <?= $post->author ?></a>
+                            <?= Carbon\Carbon::parse($post->created_at)-> diffForHumans() ?>
+                        </p>
+                    </div>
+                <?php endforeach ?>
+
+                <nav class="blog-pagination">
+                    <a class="btn btn-outline-primary" href="#">Older</a>
+                    <a class="btn btn-outline-secondary disabled" href="#">Newer</a>
+                </nav>
+
+                </div><!-- /.blog-main -->
+
+                <aside class="col-md-4 blog-sidebar">
+                <div class="p-3 mb-3 bg-light rounded">
+                    <h4 class="font-italic">About</h4>
+                    <p class="mb-0">Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+                </div>
+
+                <div class="p-3">
+                    <h4 class="font-italic">Archives</h4>
+                    <ol class="list-unstyled mb-0">
+                    <li><a href="#">March 2014</a></li>
+                    <li><a href="#">February 2014</a></li>
+                    <li><a href="#">January 2014</a></li>
+                    <li><a href="#">December 2013</a></li>
+                    <li><a href="#">November 2013</a></li>
+                    <li><a href="#">October 2013</a></li>
+                    <li><a href="#">September 2013</a></li>
+                    <li><a href="#">August 2013</a></li>
+                    <li><a href="#">July 2013</a></li>
+                    <li><a href="#">June 2013</a></li>
+                    <li><a href="#">May 2013</a></li>
+                    <li><a href="#">April 2013</a></li>
+                    </ol>
+                </div>
+
+                <div class="p-3">
+                    <h4 class="font-italic">Elsewhere</h4>
+                    <ol class="list-unstyled">
+                    <li><a href="#">GitHub</a></li>
+                    <li><a href="#">Twitter</a></li>
+                    <li><a href="#">Facebook</a></li>
+                    </ol>
+                </div>
+                </aside><!-- /.blog-sidebar -->
+
+            </div>
+        </main>
+
+        <footer class="blog-footer">
+        <p>Blog template built for <a href="https://getbootstrap.com/">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+        <p>
+            <a href="#">Back to top</a>
+        </p>
+        </footer>
+
+    </body>
+</html>
